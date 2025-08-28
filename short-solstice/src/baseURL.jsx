15fa3825269f04bfeeb516dfeead5fe6350c4405
Leo Marquez <marquez.leo100@gmail.com>
2025-08-28 00:24:48 -0700

    modified baseURL

--- short-solstice/src/baseURL.jsx
+++ short-solstice/src/baseURL.jsx
@@ -5,7 +5,8 @@ export default function baseURL() {
         // run a code space on main
         // command: npm run start:prod
         // go to ports tab in terminal and make the 3000 port public under the visibility column
-        baseURL: "https://obscure-fishstick-g4qr6jx547rpf9644-3000.app.github.dev/",
+        // baseURL: "https://obscure-fishstick-g4qr6jx547rpf9644-3000.app.github.dev/",
+        baseURL: "https://talentlink-backend-75gc.onrender.com/",
         //replace with own uri
     });
     return api;
